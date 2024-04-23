b03a5b7e82554597c43456039ce6eac2176e5c76
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-04-23 01:25:46 -0500

    Create index.js

---
+++ components/products/index.js
@@ -0,0 +1,8 @@
+// File: /components/products/index.js
+
+// Re-export all necessary components
+export { default as SpinnerMessage } from './SpinnerMessage';
+export { default as BotMessage } from './BotMessage';
+export { default as UserMessage } from './UserMessage';
+export { default as SystemMessage } from './SystemMessage';
+export { SpinnerMessage, BotMessage, UserMessage, SystemMessage } from './message';
