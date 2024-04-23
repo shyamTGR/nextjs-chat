9c1ee1c0ff3ac168cd0ddc3e28a5a2f92b437f25
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-04-23 01:27:07 -0500

    Update index.js

--- components/products/index.js
+++ components/products/index.js
@@ -5,4 +5,4 @@ export { default as SpinnerMessage } from './SpinnerMessage';
 export { default as BotMessage } from './BotMessage';
 export { default as UserMessage } from './UserMessage';
 export { default as SystemMessage } from './SystemMessage';
-export { SpinnerMessage, BotMessage, UserMessage, SystemMessage } from './message';
+
