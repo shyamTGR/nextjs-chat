c5116f79155f1ddc59f49c2b7a7573b69f828e49
Shyam Sunder <syamsunderthambabathula@gmail.com>
2024-04-23 01:22:33 -0500

    Create message.js

---
+++ components/products/message.js
@@ -0,0 +1,15 @@
+// File: /components/products/message.js
+
+// Import individual message components
+import SpinnerMessage from './SpinnerMessage';
+import BotMessage from './BotMessage';
+import UserMessage from './UserMessage';
+import SystemMessage from './SystemMessage';
+
+// Export all components from this file
+export {
+  SpinnerMessage,
+  BotMessage,
+  UserMessage,
+  SystemMessage
+};
